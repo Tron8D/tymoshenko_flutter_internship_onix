1f6eb1842e3d0edcf060292802de4e5d66de1abf
Tron8D <dima8b94@gmail.com>
2021-12-15 20:24:17 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutter_intership_onix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_intership_onix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/models/currency.dart
@@ -0,0 +1,14 @@
+//base class for currency
+class Currency {
+  final String name;
+  final String symbol;
+  final double rateToUah;
+
+  Currency({
+    required this.name,
+    required this.symbol,
+    required this.rateToUah,
+  }) {
+    assert(rateToUah > 0);
+  }
+}
